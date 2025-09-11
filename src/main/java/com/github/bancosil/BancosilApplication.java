46243f592b041aa85ab981c56a4ba4827b209a9a
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-10 22:03:19 -0300

    intial: bancosil structure

---
+++ src/main/java/com/github/bancosil/BancosilApplication.java
@@ -0,0 +1,13 @@
+package com.github.bancosil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancosilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancosilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/bancosil/BancosilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.bancosil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancosilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
