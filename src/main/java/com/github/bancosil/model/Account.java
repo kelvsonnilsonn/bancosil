5131b6cc7c45fe404de682123be9aa6492af5483
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-12 15:28:28 -0300

    fix: initialize money and address with default values in Account constructor
    - rename "name" to "username"
    - Set money to BigDecimal.ZERO to prevent null values
    - Add default "Não definido" address values
    - Provide selectAddress method for later customization

--- src/main/java/com/github/bancosil/model/Account.java
+++ src/main/java/com/github/bancosil/model/Account.java
@@ -2,14 +2,18 @@ package com.github.bancosil.model;
 
 import com.github.bancosil.model.valueobjects.*;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 @Getter
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "tipo_conta", discriminatorType = DiscriminatorType.STRING)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public abstract class Account {
 
     @Id
@@ -17,7 +21,7 @@ public abstract class Account {
     private Long id;
 
     @Embedded
-    private Username name;
+    private Username username;
 
     @Embedded
     private Password password;
@@ -33,6 +37,19 @@ public abstract class Account {
     private Address address;
 
     private BigDecimal money;
+
+    public Account(String username, String password, String email, String cpf){
+        this.username = new Username(username);
+        this.password = new Password(password);
+        this.email = new Email(email);
+        this.cpf = new CPF(cpf);
+        this.address = new Address("Não definida", "Não definida", "Não definida", 0);
+        this.money = BigDecimal.ZERO;
+    }
+
+    public void selectAddress(String city, String street, String zipcode, int number){
+        this.address = Address.putAddress(city, street, zipcode, number);
+    }
 
     public BigDecimal withdraw(BigDecimal amount) {
         this.money = money.subtract(amount);
