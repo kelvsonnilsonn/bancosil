f0373ec69ef7188db91fae49432878e7c2c718a9
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-12 18:36:18 -0300

    test(service): trying to log-in account and execute bank operations.

--- src/test/java/com/github/bancosil/service/OperationalServiceTest.java
+++ src/test/java/com/github/bancosil/service/OperationalServiceTest.java
@@ -1,5 +1,7 @@
 package com.github.bancosil.service;
 
+import com.github.bancosil.config.AccountConfigurations;
+import com.github.bancosil.model.Account;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,15 +19,35 @@ class OperationalServiceTest {
     @Autowired
     private AccountService serviceAc;
 
+    @Autowired
+    private AccountConfigurations accountConfigurations;
+
     @Test
     void deveDepositarUmValorValido() {
-        assertDoesNotThrow(() -> service.deposit(2L, BigDecimal.valueOf(120)));
+        assertDoesNotThrow(() -> accountConfigurations.login("Kelvson", "1234"));
+        Account conta = accountConfigurations.getCurrentUser();
+        assertDoesNotThrow(() -> service.deposit(conta, BigDecimal.valueOf(100)));
+        accountConfigurations.logout();
+        serviceAc.update(conta);
     }
 
     @Test
-    void deveRealizarUmSaqueComSucesso() {
-        assertDoesNotThrow(() -> service.withdraw(2l, BigDecimal.valueOf(200)));
-        assertEquals(0, BigDecimal.valueOf(60).compareTo(serviceAc.findById(2L).getMoney()));
+    void deveEstarDeslogado(){
+        assertFalse(accountConfigurations.isLogged());
     }
 
+    @Test
+    void valorDeveEstarGuardadoNoBancoDeDados(){
+        Account conta = serviceAc.findById(2L);
+        assertEquals(0, BigDecimal.valueOf(160).compareTo(conta.getMoney()));
+    }
+
+    @Test
+    void deveSacarDinheiro(){
+        Account conta = serviceAc.findById(2L);
+        BigDecimal valor = service.withdraw(conta, BigDecimal.valueOf(100));
+        assertEquals(0, BigDecimal.valueOf(100)
+                .compareTo(valor));
+        serviceAc.update(conta);
+    }
 }
