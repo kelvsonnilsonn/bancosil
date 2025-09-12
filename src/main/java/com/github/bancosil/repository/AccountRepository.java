a8ee588bd10df132ebe3d9b60e50b0a17b4a0147
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-12 15:29:51 -0300

    feat(repository): implement account login query method

--- src/main/java/com/github/bancosil/repository/AccountRepository.java
+++ src/main/java/com/github/bancosil/repository/AccountRepository.java
@@ -2,6 +2,13 @@ package com.github.bancosil.repository;
 
 import com.github.bancosil.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    @Query("SELECT a FROM Account a WHERE a.username.username = :username AND a.password.password =:password")
+    Optional<Account> findByUsernameAndPassword(@Param("username") String username, @Param("password") String password);
 }
