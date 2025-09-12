49e454b556ddc576793719efc69ad4f6f0a2f768
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-12 18:19:50 -0300

    feat: add Withdraw and Deposit operations classes to separate responsabilities and have control between others operations.

---
+++ src/main/java/com/github/bancosil/service/operation/Withdraw.java
@@ -0,0 +1,14 @@
+package com.github.bancosil.service.operation;
+
+import com.github.bancosil.model.Account;
+import com.github.bancosil.service.validator.OperationValidator;
+
+import java.math.BigDecimal;
+
+public class Withdraw implements Operation{
+    @Override
+    public void execute(Account account, BigDecimal amount) {
+        OperationValidator.validate(amount);
+        account.withdraw(amount);
+    }
+}
