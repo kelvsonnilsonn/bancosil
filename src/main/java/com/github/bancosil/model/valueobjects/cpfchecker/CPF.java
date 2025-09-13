4ce069fe0d07f3241cae914cd840f4d4b6e03114
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-13 14:50:02 -0300

    refactor: remove validate method to use separated classes to validate the formatter and the unicity of the CPF input
    - moved CPF class to cpfchecker package which contains CPFFormatter and CPFCalculateDigits

---
+++ src/main/java/com/github/bancosil/model/valueobjects/cpfchecker/CPF.java
@@ -0,0 +1,22 @@
+package com.github.bancosil.model.valueobjects.cpfchecker;
+
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CPF {
+    private String cpf;
+
+    public CPF(String cpf){
+        this.cpf = CPFFormatter.validateFormatter(cpf);;
+        CPFCalculateDigits.validateDigits(this.cpf);
+    }
+
+    public static CPF of(String cpf){
+        return new CPF(cpf);
+    }
+}
