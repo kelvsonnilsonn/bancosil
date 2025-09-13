75ed366d9244d9343dd5259cddc6c768d0b15e36
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-13 12:40:22 -0300

    refactor: deleted OperationRepository.java because it wasn't be used.

--- src/main/java/com/github/bancosil/repository/OperationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.bancosil.repository;
-
-import com.github.bancosil.model.OperationalLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OperationRepository extends JpaRepository<OperationalLog, Long> {
-}
