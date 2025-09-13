b9a22d104a68fcba337d3e15fa7e101d33153dcd
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-13 12:37:50 -0300

    feat: add REGEX for represents a valid Zipcode input.
    - add validate method for validate Zipcode REGEX and null inputs.

--- src/main/java/com/github/bancosil/model/valueobjects/ZipCode.java
+++ src/main/java/com/github/bancosil/model/valueobjects/ZipCode.java
@@ -1,11 +1,13 @@
 package com.github.bancosil.model.valueobjects;
 
+import com.github.bancosil.exception.valueobjects.InvalidZipcodeNumberException;
 import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.Objects;
+import java.util.regex.Pattern;
 
 @Embeddable
 @Getter
@@ -13,11 +15,22 @@ import java.util.Objects;
 public class ZipCode {
     private String zipcode;
 
+    private static final String ZIP_CODE_REGEX = "^\\d{5,}-?\\d{3,}$";
+    private static final Pattern pattern = Pattern.compile(ZIP_CODE_REGEX);
+
     public ZipCode(String zipcode){
-        this.zipcode = Objects.requireNonNull(zipcode);;
+        this.zipcode = validate(zipcode);
     }
 
     public static ZipCode of(String zipcode){
         return new ZipCode(zipcode);
+    }
+
+    private String validate(String zipcode){
+        Objects.requireNonNull(zipcode, "O CEP não pode ser nulo.");
+        if(!pattern.matcher(zipcode).matches()){
+            throw new InvalidZipcodeNumberException();
+        }
+        return zipcode;
     }
 }
