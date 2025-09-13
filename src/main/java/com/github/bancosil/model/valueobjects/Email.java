3e5a88d2c0c2955fca4c98ae524c8f8867f2a565
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-13 12:34:15 -0300

    feat: add REGEX for represents a valid Email input.
    - add validate method for validate Email REGEX and null inputs.

--- src/main/java/com/github/bancosil/model/valueobjects/Email.java
+++ src/main/java/com/github/bancosil/model/valueobjects/Email.java
@@ -1,11 +1,13 @@
 package com.github.bancosil.model.valueobjects;
 
+import com.github.bancosil.exception.valueobjects.InvalidEmailException;
 import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.Objects;
+import java.util.regex.Pattern;
 
 @Embeddable
 @Getter
@@ -13,11 +15,26 @@ import java.util.Objects;
 public class Email {
     private String email;
 
+    private static final String EMAIL_REGEX =
+            "^[a-zA-Z]+[a-zA-Z0-9._]+" +
+            "@[a-zA-Z]+[a-zA-Z0-9._]" +
+            "+\\.[a-zA-Z]{2,}$";
+
+    private static final Pattern pattern = Pattern.compile(EMAIL_REGEX);
+
     public Email(String email){
-        this.email = Objects.requireNonNull(email);;
+        this.email = validate(email);
     }
 
     public static Email of(String email){
         return new Email(email);
+    }
+
+    private String validate(String email){
+        Objects.requireNonNull(email, "O Email não pode ser nulo.");
+        if(!pattern.matcher(email).matches()){
+            throw new InvalidEmailException();
+        }
+        return email;
     }
 }
