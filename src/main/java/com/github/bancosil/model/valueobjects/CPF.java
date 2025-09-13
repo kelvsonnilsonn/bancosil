ed9373a45f83a28cea3b17154a613f43e117ac60
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-13 12:33:12 -0300

    feat: add REGEX for represents a valid CPF.
    - add validate method for validate CPF REGEX and null inputs.

--- src/main/java/com/github/bancosil/model/valueobjects/CPF.java
+++ src/main/java/com/github/bancosil/model/valueobjects/CPF.java
@@ -1,11 +1,13 @@
 package com.github.bancosil.model.valueobjects;
 
+import com.github.bancosil.exception.valueobjects.InvalidCPFNumberException;
 import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.Objects;
+import java.util.regex.Pattern;
 
 @Embeddable
 @Getter
@@ -13,11 +15,23 @@ import java.util.Objects;
 public class CPF {
     private String cpf;
 
+    private static final String CPF_REGEX = "^\\d{3}.?\\d{3}.?\\d{3}-?\\d{2}$";
+
+    private static final Pattern pattern = Pattern.compile(CPF_REGEX);
+
     public CPF(String cpf){
-        this.cpf = Objects.requireNonNull(cpf);
+        this.cpf = validate(cpf);
     }
 
     public static CPF of(String cpf){
         return new CPF(cpf);
+    }
+
+    private String validate(String cpf){
+        Objects.requireNonNull(cpf, "O CPF não pode ser nulo.");
+        if(!pattern.matcher(cpf).matches()){
+            throw new InvalidCPFNumberException();
+        }
+        return cpf;
     }
 }
