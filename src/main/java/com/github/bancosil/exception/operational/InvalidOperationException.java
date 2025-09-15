f5bb7671b9be77885880877d79308c99237e1ac7
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-15 15:45:47 -0300

    feat: add personalized exception InvalidOperationException for represents an operation error.

---
+++ src/main/java/com/github/bancosil/exception/operational/InvalidOperationException.java
@@ -0,0 +1,7 @@
+package com.github.bancosil.exception.operational;
+
+public class InvalidOperationException extends RuntimeException {
+    public InvalidOperationException(String message) {
+        super(message);
+    }
+}
