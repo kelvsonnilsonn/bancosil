6c6ad2aecd9fde49e2ad9e8da8cb4ae4c8e0c70a
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-15 15:42:04 -0300

    refactor: removed because it's not being used.

--- src/main/java/com/github/bancosil/exception/operational/ZeroAttemptException.java
+++
@@ -1,8 +0,0 @@
-package com.github.bancosil.exception.operational;
-
-public class ZeroAttemptException extends RuntimeException {
-    public ZeroAttemptException() { super("Uma operação envolvendo o valor R$0.0 é ignorada."); }
-    public ZeroAttemptException(String message) {
-        super(message);
-    }
-}
