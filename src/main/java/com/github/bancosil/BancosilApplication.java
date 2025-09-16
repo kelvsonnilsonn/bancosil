8984ca22b5f1096f92b3daf25d8fb0e3ac5bef47
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-16 17:36:03 -0300

    fix: add CommandLineRunner run method for run program.

--- src/main/java/com/github/bancosil/BancosilApplication.java
+++ src/main/java/com/github/bancosil/BancosilApplication.java
@@ -1,7 +1,10 @@
 package com.github.bancosil;
 
+import com.github.bancosil.application.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BancosilApplication {
@@ -9,5 +12,12 @@ public class BancosilApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BancosilApplication.class, args);
 	}
+
+    @Bean
+    public CommandLineRunner run(ApplicationRunner runner){
+        return args -> {
+            runner.start();
+        };
+    }
 
 }
