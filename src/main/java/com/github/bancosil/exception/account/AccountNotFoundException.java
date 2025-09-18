6dc8d5f91c2965f0b9e89af7b0620f3b470f4a58
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-18 12:23:47 -0300

    fix: changed message

--- src/main/java/com/github/bancosil/exception/account/AccountNotFoundException.java
+++ src/main/java/com/github/bancosil/exception/account/AccountNotFoundException.java
@@ -2,7 +2,7 @@ package com.github.bancosil.exception.account;
 
 public class AccountNotFoundException extends RuntimeException {
     public AccountNotFoundException(){ super("Conta com não encontrada.");}
-    public AccountNotFoundException(Long id){ super("Conta com ID: " + id + " não encontrada.");}
+    public AccountNotFoundException(Long id){ super("Conta com ID " + id + " não encontrada.");}
     public AccountNotFoundException(String message) {
         super(message);
     }
