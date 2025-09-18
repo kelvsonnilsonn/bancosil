d6e67abfe266e111f46c38df0de179afc87e1f8e
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-18 10:53:06 -0300

    feat: create findByUsername method for find all accounts with username starting with username passed in parameter.

--- src/main/java/com/github/bancosil/repository/AccountRepository.java
+++ src/main/java/com/github/bancosil/repository/AccountRepository.java
@@ -5,10 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
     @Query("SELECT a FROM Account a WHERE a.username.username = :username AND a.password.password =:password")
     Optional<Account> findByUsernameAndPassword(@Param("username") String username, @Param("password") String password);
+
+    @Query("SELECT a FROM Account a WHERE a.username.username LIKE CONCAT (:username, '%')")
+    List<Account> findByUsername(@Param("username") String username);
 }
