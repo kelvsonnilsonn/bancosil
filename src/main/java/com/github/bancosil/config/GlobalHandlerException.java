fc77fc82ba70f96f6150237561dff63084a971b3
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-18 12:12:55 -0300

    feat: add DataIntegrityException catcher for manage duplicated CPF.

--- src/main/java/com/github/bancosil/config/GlobalHandlerException.java
+++ src/main/java/com/github/bancosil/config/GlobalHandlerException.java
@@ -6,6 +6,7 @@ import com.github.bancosil.exception.valueobjects.InvalidEmailException;
 import com.github.bancosil.exception.valueobjects.ShortUsernameException;
 import com.github.bancosil.exception.valueobjects.password.InvalidPasswordException;
 import com.github.bancosil.exception.valueobjects.password.ShortPasswordException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -44,9 +45,18 @@ public class GlobalHandlerException {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDataIntegrityViolation(DataIntegrityViolationException e) {
+        if (e.getMessage().toLowerCase().contains("cpf")) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("CPF já está em uso");
+        }
+        return ResponseEntity.status(HttpStatus.CONFLICT).body("Dados duplicados");
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGenericException(Exception e){
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body("An unexpected error occurred: " + e.getMessage());
     }
+
 }
