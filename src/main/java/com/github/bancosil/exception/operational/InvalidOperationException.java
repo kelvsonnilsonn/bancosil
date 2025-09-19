0d8d9550ac027258ca52a995c23b41e8878623a4
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-18 21:48:48 -0300

    refactor(exception): remove InvalidOperationException as it was redundant
    - Delete InvalidOperationException.java file from codebase
    - The exception was not being used in any business logic
    - Simplify exception hierarchy by removing unused components

--- src/main/java/com/github/bancosil/exception/operational/InvalidOperationException.java
+++
@@ -1,7 +0,0 @@
-package com.github.bancosil.exception.operational;
-
-public class InvalidOperationException extends RuntimeException {
-    public InvalidOperationException(String message) {
-        super(message);
-    }
-}
