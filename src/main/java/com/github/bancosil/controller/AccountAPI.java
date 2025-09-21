83bfb3a4a26064d828a954d4bdbb4b77727fd06a
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-21 11:59:52 -0300

    feat: add Swagger annotations to document AccountAPI, interface which define all AccountController methods.

--- src/main/java/com/github/bancosil/controller/AccountAPI.java
+++ src/main/java/com/github/bancosil/controller/AccountAPI.java
@@ -1,6 +1,11 @@
 package com.github.bancosil.controller;
 
+import com.github.bancosil.config.AppConstants;
+import com.github.bancosil.config.HttpConstants;
 import com.github.bancosil.dto.AccountDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,10 +13,37 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
+@Tag(name="Accounts", description = "Gestão de contas de usuários")
 public interface AccountAPI {
+
+    @Operation(summary = "Criar nova conta", description = "Cria uma nova conta do usuário.")
+    @ApiResponse(responseCode = HttpConstants.CREATED, description = "Conta criada com sucesso")
+    @ApiResponse(responseCode = HttpConstants.BAD_REQUEST, description = HttpConstants.BAD_REQUEST_MSG)
+    @ApiResponse(responseCode = HttpConstants.CONFLICT, description = "Conflito - conta já existe")
+    @ApiResponse(responseCode = HttpConstants.SERVER_ERROR, description = HttpConstants.INTERN_SERVER_ERROR_MSG)
     ResponseEntity<AccountDTO> create(@RequestBody AccountDTO user);
+
+    @Operation(summary = "Deletar uma conta", description = "Deleta uma conta de usuário.")
+    @ApiResponse(responseCode = HttpConstants.OK, description = "Conta deletada com sucesso")
+    @ApiResponse(responseCode = HttpConstants.NOT_FOUND, description = HttpConstants.NOT_FOUND_MSG)
+    @ApiResponse(responseCode = HttpConstants.SERVER_ERROR, description = HttpConstants.INTERN_SERVER_ERROR_MSG)
     ResponseEntity<String> delete(@PathVariable("id") Long id);
+
+    @Operation(summary = "Procurar uma conta por ID", description = "Retorna uma conta específica.")
+    @ApiResponse(responseCode = HttpConstants.OK, description = "Conta encontrada")
+    @ApiResponse(responseCode = HttpConstants.NOT_FOUND, description = HttpConstants.NOT_FOUND_MSG)
+    @ApiResponse(responseCode = HttpConstants.SERVER_ERROR, description = HttpConstants.INTERN_SERVER_ERROR_MSG)
     ResponseEntity<AccountDTO> findById(@PathVariable("id") Long id);
-    ResponseEntity<List<AccountDTO>> findByUsername(@RequestParam("username") String username);
+
+    @Operation(summary = "Buscar por username", description = "Busca contas por username")
+    @ApiResponse(responseCode = HttpConstants.OK, description = "Contas encontradas")
+    @ApiResponse(responseCode = HttpConstants.NO_CONTENT, description = HttpConstants.NO_CONTENT_MSG)
+    @ApiResponse(responseCode = HttpConstants.SERVER_ERROR, description = HttpConstants.INTERN_SERVER_ERROR_MSG)
+    ResponseEntity<List<AccountDTO>> findByUsername(@RequestParam(AppConstants.USERNAME_PARAM) String username);
+
+    @Operation(summary = "Listar todas as contas", description = "Retorna todas as contas com paginação")
+    @ApiResponse(responseCode = HttpConstants.OK, description = "Lista de contas")
+    @ApiResponse(responseCode = HttpConstants.NO_CONTENT, description = HttpConstants.NO_CONTENT_MSG)
+    @ApiResponse(responseCode = HttpConstants.SERVER_ERROR, description = HttpConstants.INTERN_SERVER_ERROR_MSG)
     ResponseEntity<List<AccountDTO>> findAll();
 }
