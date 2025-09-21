e282ec2c589ddb03832b469ef900c0dd3046a2eb
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-21 08:50:37 -0300

    feat: add an application constants class who store all constants used in the program.
    - remove "magic strings" scattered in the program, because it's centralize all the strings in one static and final class.
    
    - use @UtilityClass lombok annotation to make this class can't be instanced.

--- src/main/java/com/github/bancosil/config/AppConstants.java
+++ src/main/java/com/github/bancosil/config/AppConstants.java
@@ -11,9 +11,21 @@ public class AppConstants {
     public static final String SEARCH_PATH = "/search";
     public static final String DELETE_PATH = "/{id}";
 
+    public static final String AUTH_BASE_PATH = "/auth";
+    public static final String LOGIN_PATH = "/login";
+    public static final String LOGOUT_PATH = "/logout";
+
+    public static final String OPERATION_BASE_PATH = "/operation";
+    public static final String DEPOSIT_PATH = "/deposit";
+    public static final String WITHDRAW_PATH = "/withdraw";
+    public static final String TRANSFER_PATH = "/transfer";
+
     public static final String USERNAME_PARAM = "username";
 
     public static final String ACCOUNT_DELETED_MSG = "A conta %s foi deletada";
+    public static final String DEPOSIT_MSG = "Deposito de R$%.2f realizado com sucesso";
+    public static final String WITHDRAW_MSG = "Saque de R$%.2f realizado com sucesso";
+    public static final String TRANSFER_MSG = "Transferência de R$%.2f para %s realizada com sucesso";
 
 
 
