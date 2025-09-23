630cfef26afa734ef2fde9ea4ccc8afe2a62ae6d
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-23 16:39:00 -0300

    refactor: modified @RequestParam to @RequestBody LoginDTO in login method
    - change @ApiResponse(responsecode ...) from HttpConstants to a hardcoded message for swagger.

--- src/main/java/com/github/bancosil/controller/AuthAPI.java
+++ src/main/java/com/github/bancosil/controller/AuthAPI.java
@@ -1,22 +1,23 @@
 package com.github.bancosil.controller;
 
-import com.github.bancosil.config.AppConstants;
 import com.github.bancosil.config.HttpConstants;
 import com.github.bancosil.dto.AccountDTO;
+import com.github.bancosil.dto.LoginDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name="Authentication", description = "Operações de autenticação de usuário")
 public interface AuthAPI {
 
     @Operation(summary = "Login de usuário", description = "Autentica um usuário no sistema")
     @ApiResponse(responseCode = HttpConstants.OK, description = "Login realizado com sucesso")
+    @ApiResponse(responseCode = HttpConstants.UNAUTHORIZED, description = "Credenciais inválidas")
     @ApiResponse(responseCode = HttpConstants.BAD_REQUEST, description = HttpConstants.BAD_REQUEST_MSG)
     @ApiResponse(responseCode = HttpConstants.SERVER_ERROR, description = HttpConstants.INTERN_SERVER_ERROR_MSG)
-    ResponseEntity<AccountDTO> login(@RequestParam String username, @RequestParam String password);
+    ResponseEntity<AccountDTO> login(@RequestBody LoginDTO loginDTO);
 
     @Operation(summary = "Logout de usuário", description = "Desconecta um usuário do sistema")
     @ApiResponse(responseCode = HttpConstants.OK, description = "Logout realizado com sucesso")
