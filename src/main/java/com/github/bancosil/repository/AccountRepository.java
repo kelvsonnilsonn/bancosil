6fbfcb660d605576a29fd0f2896e0f9620c4b272
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-23 15:05:54 -0300

    refactor(repository): add Page<Account> modifier to make findByUsername return a Page, not a List.

--- src/main/java/com/github/bancosil/repository/AccountRepository.java
+++ src/main/java/com/github/bancosil/repository/AccountRepository.java
@@ -1,6 +1,8 @@
 package com.github.bancosil.repository;
 
 import com.github.bancosil.model.Account;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,6 +15,6 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
     @Query("SELECT a FROM Account a WHERE a.username.username = :username AND a.password.password =:password")
     Optional<Account> findByUsernameAndPassword(@Param("username") String username, @Param("password") String password);
 
-    @Query("SELECT a FROM Account a WHERE a.username.username LIKE CONCAT (:username, '%')")
-    List<Account> findByUsername(@Param("username") String username);
+    @Query("SELECT a FROM Account a WHERE LOWER(a.username.username) LIKE CONCAT (:username, '%')")
+    Page<Account> findByUsername(@Param("username") String username, Pageable pageable);
 }
