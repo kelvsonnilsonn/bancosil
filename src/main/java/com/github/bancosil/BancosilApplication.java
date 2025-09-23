c37d664a0e3c3a6dc659b467ce6093983f54d29c
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-23 17:13:45 -0300

    feat: removed ApplicationRunner.java class because we aren't using console anymore

--- src/main/java/com/github/bancosil/BancosilApplication.java
+++ src/main/java/com/github/bancosil/BancosilApplication.java
@@ -1,6 +1,5 @@
 package com.github.bancosil;
 
-import com.github.bancosil.application.ApplicationRunner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
