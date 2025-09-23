358623bab13c9593d63afdc752d770e4dca6f4b5
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-23 16:47:28 -0300

    refactor(controller): use @RequestBody instead of @RequestParam annotation to store in LoginDTO

--- src/main/java/com/github/bancosil/controller/AuthController.java
+++ src/main/java/com/github/bancosil/controller/AuthController.java
@@ -3,12 +3,19 @@ package com.github.bancosil.controller;
 import com.github.bancosil.config.AccountConfigurations;
 import com.github.bancosil.config.AppConstants;
 import com.github.bancosil.dto.AccountDTO;
+import com.github.bancosil.dto.LoginDTO;
 import com.github.bancosil.model.Account;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Por: Kelvson Nilson
+ * Última atualização: 23/09/2025
+ * Versão: 1.1
+ * */
 
 @RestController
 @RequestMapping(AppConstants.AUTH_BASE_PATH)
@@ -22,9 +29,8 @@ public class AuthController implements AuthAPI{
 
     @Override
     @PostMapping(AppConstants.LOGIN_PATH)
-    public ResponseEntity<AccountDTO> login(@RequestParam String username,
-                                            @RequestParam String password) {
-        accountConfigurations.login(username, password);
+    public ResponseEntity<AccountDTO> login(@RequestBody LoginDTO loginDTO) {
+        accountConfigurations.login(loginDTO.username(), loginDTO.password());
         Account account = accountConfigurations.getCurrentUser();
         return ResponseEntity.ok(AccountConverter.convert(account));
     }
