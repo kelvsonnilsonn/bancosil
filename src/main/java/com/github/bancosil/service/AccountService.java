84c86bce78a561114585c88ff46bde9801475752
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-27 15:27:35 -0300

    refactor(service): update return type of create and delete from void to AccountResponseDTO and String.
    - Use AccountMapper to map Account to AccountResponseDTO
    - Use 'fromPage' method to convert a Page<> into a PageResponseDTO

--- src/main/java/com/github/bancosil/service/AccountService.java
+++ src/main/java/com/github/bancosil/service/AccountService.java
@@ -1,9 +1,14 @@
 package com.github.bancosil.service;
 
 import com.github.bancosil.config.LoginSystem;
+import com.github.bancosil.dto.AccountRequestDTO;
+import com.github.bancosil.dto.AccountResponseDTO;
+import com.github.bancosil.dto.PageResponseDTO;
 import com.github.bancosil.exception.account.AccountNotFoundException;
+import com.github.bancosil.mapper.AccountMapper;
 import com.github.bancosil.model.Account;
 import com.github.bancosil.repository.AccountRepository;
+import com.github.bancosil.util.AppConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
@@ -11,7 +16,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
-import java.util.Objects;
 
 /**
  * Por: Kelvson Nilson
@@ -21,48 +25,57 @@ import java.util.Objects;
 @Service
 public class AccountService implements LoginSystem {
 
-    private final AccountRepository accountDAO;
+    private final AccountRepository accountRepository;
 
     @Autowired
-    public AccountService(AccountRepository accountDAO){
-        this.accountDAO = accountDAO;
+    public AccountService(AccountRepository accountRepository){
+        this.accountRepository = accountRepository;
     }
 
-    public void create(Account account){
-        Objects.requireNonNull(account, "A conta não pode ser nula.");
-        accountDAO.save(account);
+    public AccountResponseDTO create(AccountRequestDTO account){
+        Account receivedAccount = AccountMapper.convert(account);
+        Account savedAccount = accountRepository.save(receivedAccount);
+        return AccountMapper.toResponse(savedAccount);
     }
 
-    public void delete(Long id){
+    public String delete(Long id){
         try{
-            accountDAO.deleteById(id);
+            Account account = findEntityById(id);
+            String message = String.format(AppConstants.ACCOUNT_DELETED_MSG, account.getUsername());
+            accountRepository.deleteById(id);
+            return message;
         } catch (EmptyResultDataAccessException e){
             throw new AccountNotFoundException(id);
         }
     }
 
-    public Account findById(Long id){
-        Objects.requireNonNull(id, "O id não pode ser nulo.");
-        return accountDAO.findById(id)
-                .orElseThrow(() -> new AccountNotFoundException(id));
+    public Account findEntityById(Long id){
+        return accountRepository.findById(id).orElseThrow(() -> new AccountNotFoundException(id));
     }
 
-    public Page<Account> findByUsername(String username, Pageable pageable){
-        return accountDAO.findByUsername(username, pageable);
+    public AccountResponseDTO findById(Long id){
+        Account account = findEntityById(id);
+        return AccountMapper.toResponse(account);
     }
 
-    public Page<Account> findAll(Pageable pageable){
-        return accountDAO.findAll(pageable);
+    public PageResponseDTO<AccountResponseDTO> findByUsername(String username, Pageable pageable){
+        Page<AccountResponseDTO> content =  accountRepository.findByUsername(username, pageable).map(AccountMapper::toResponse);
+        return PageResponseDTO.fromPage(content);
+    }
+
+    public PageResponseDTO<AccountResponseDTO> findAll(Pageable pageable){
+        Page<AccountResponseDTO> content = accountRepository.findAll(pageable).map(AccountMapper::toResponse);
+        return PageResponseDTO.fromPage(content);
     }
 
     @Override
     public Account login(String username, String password){
-        return accountDAO.findByUsernameAndPassword(username, password)
+        return accountRepository.findByUsernameAndPassword(username, password)
                 .orElseThrow(AccountNotFoundException::new);
     }
 
     public void update(Account ...account) {
-        Arrays.stream(account).forEach(accountDAO::save);
+        Arrays.stream(account).forEach(accountRepository::save);
     }
 
 }
