3ff283aaef5eaabbd982cae3b2dbc223d93af1eb
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-27 15:38:03 -0300

    refactor(config): removed AccountConfigurations instance to clear controller

--- src/main/java/com/github/bancosil/service/OperationalService.java
+++ src/main/java/com/github/bancosil/service/OperationalService.java
@@ -1,9 +1,10 @@
 package com.github.bancosil.service;
 
+import com.github.bancosil.config.AccountConfigurations;
+import com.github.bancosil.exception.account.UnauthorizedException;
 import com.github.bancosil.exception.operational.SelfTransferException;
 import com.github.bancosil.model.Account;
 import com.github.bancosil.service.operation.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,23 +18,25 @@ public class OperationalService {
 
     private final LogService logService;
     private final AccountService accountService;
+    private final AccountConfigurations accountConfigurations;
 
-    @Autowired
-    public OperationalService(LogService logService, AccountService accountService){
+
+    public OperationalService(LogService logService, AccountService accountService, AccountConfigurations accountConfigurations){
         this.logService = logService;
         this.accountService = accountService;
+        this.accountConfigurations = accountConfigurations;
     }
 
-    public BigDecimal withdraw(Account account, BigDecimal amount){
-        executeOperation(Withdraw::new, account, amount);
-        return amount;
+    public void withdraw(BigDecimal amount){
+        executeOperation(Withdraw::new, getCurrentUser(), amount);
     }
 
-    public void deposit(Account account, BigDecimal amount){
-        executeOperation(Deposit::new, account, amount);
+    public void deposit(BigDecimal amount){
+        executeOperation(Deposit::new,  getCurrentUser(), amount);
     }
 
-    public void transferPix(Account sender, Account receiver, BigDecimal amount){
+    public void transferPix(Account receiver, BigDecimal amount){
+        Account sender = getCurrentUser();
         if(Objects.equals(sender.getId(), receiver.getId())){
             throw new SelfTransferException("Você não pode transferir para si mesmo");
         }
@@ -52,5 +55,12 @@ public class OperationalService {
         operation.execute(sender, receiver, amount);
         accountService.update(sender, receiver);
         logService.register(operation.operationType(), sender, receiver, amount);
+    }
+
+    private Account getCurrentUser(){
+        if(!accountConfigurations.isLogged()){
+            throw new UnauthorizedException();
+        }
+        return accountConfigurations.getCurrentUser();
     }
 }
