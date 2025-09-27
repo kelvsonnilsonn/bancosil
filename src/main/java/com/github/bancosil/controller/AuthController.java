38b75b5c90728e5c263775bb6c1a42ab4970e88e
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-27 11:12:02 -0300

    refactor(controller): add Mapper to convert Account into DTO

--- src/main/java/com/github/bancosil/controller/AuthController.java
+++ src/main/java/com/github/bancosil/controller/AuthController.java
@@ -1,7 +1,8 @@
 package com.github.bancosil.controller;
 
 import com.github.bancosil.config.AccountConfigurations;
-import com.github.bancosil.config.AppConstants;
+import com.github.bancosil.mapper.AccountMapper;
+import com.github.bancosil.util.AppConstants;
 import com.github.bancosil.dto.AccountDTO;
 import com.github.bancosil.dto.LoginDTO;
 import com.github.bancosil.model.Account;
@@ -10,12 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-/**
- * Por: Kelvson Nilson
- * Última atualização: 23/09/2025
- * Versão: 1.1
- * */
 
 @RestController
 @RequestMapping(AppConstants.AUTH_BASE_PATH)
@@ -32,7 +27,7 @@ public class AuthController implements AuthAPI{
     public ResponseEntity<AccountDTO> login(@RequestBody LoginDTO loginDTO) {
         accountConfigurations.login(loginDTO.username(), loginDTO.password());
         Account account = accountConfigurations.getCurrentUser();
-        return ResponseEntity.ok(AccountConverter.convert(account));
+        return ResponseEntity.ok(AccountMapper.convert(account));
     }
 
     @Override
