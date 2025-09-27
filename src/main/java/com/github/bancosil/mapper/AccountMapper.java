b42dbda1a929ac0b80290ee513e3f62ea4e09de5
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-09-27 15:22:03 -0300

    feat(mapper): add toResponse method which convert Account to AccountResponseDTO

--- src/main/java/com/github/bancosil/mapper/AccountMapper.java
+++ src/main/java/com/github/bancosil/mapper/AccountMapper.java
@@ -1,15 +1,16 @@
 package com.github.bancosil.mapper;
 
-import com.github.bancosil.dto.AccountDTO;
+import com.github.bancosil.dto.AccountRequestDTO;
+import com.github.bancosil.dto.AccountResponseDTO;
 import com.github.bancosil.model.Account;
+import com.github.bancosil.model.Corrente;
 
 public class AccountMapper {
-    public static AccountDTO convert(Account account){
-        return new AccountDTO(
-                account.getUsername(),
-                account.getEmail(),
-                null,
-                account.getCPF()
-        );
+    public static Account convert(AccountRequestDTO account){
+        return new Corrente(account.username(), account.password(), account.email(), account.cpf());
+    }
+
+    public static AccountResponseDTO toResponse(Account account){
+        return new AccountResponseDTO(account.getId(), account.getUsername(), account.getEmail(), account.getCreatedAt());
     }
 }
