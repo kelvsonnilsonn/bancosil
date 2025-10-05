22a710f18b6d9998948ba41fc210c5772230bc7e
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-05 13:39:49 -0300

    refactor(controller): add @RequiredArgsConstructor to remove explicit constructor in AccountController

--- src/main/java/com/github/bancosil/controller/AccountController.java
+++ src/main/java/com/github/bancosil/controller/AccountController.java
@@ -4,19 +4,17 @@ import com.github.bancosil.dto.AccountResponseDTO;
 import com.github.bancosil.dto.PageResponseDTO;
 import com.github.bancosil.service.AccountService;
 import com.github.bancosil.util.AppConstants;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(AppConstants.ACCOUNT_BASE_PATH)
+@RequiredArgsConstructor
 public class AccountController implements AccountAPI{
 
     private final AccountService accountService;
-
-    public AccountController(AccountService accountService){
-        this.accountService = accountService;
-    }
 
     @DeleteMapping(AppConstants.ID_PATH)
     public ResponseEntity<String> delete(@PathVariable Long id){
