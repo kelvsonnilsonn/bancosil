2cbfb242ca521cbb6c2728e4a0c0851dc0284574
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-05 12:31:45 -0300

    feat(util): add '/register' path in AppConstants

--- src/main/java/com/github/bancosil/util/AppConstants.java
+++ src/main/java/com/github/bancosil/util/AppConstants.java
@@ -8,13 +8,12 @@ public class AppConstants {
     // Paths
 
     public static final String ACCOUNT_BASE_PATH = "/accounts";
-    public static final String CREATE_PATH = "/create";
     public static final String ID_PATH = "/{id}";
     public static final String SEARCH_PATH = "/search";
 
     public static final String AUTH_BASE_PATH = "/auth";
     public static final String LOGIN_PATH = "/login";
-    public static final String LOGOUT_PATH = "/logout";
+    public static final String REGISTER_PATH = "/register";
 
     public static final String OPERATION_BASE_PATH = "/operation";
     public static final String DEPOSIT_PATH = "/deposit";
