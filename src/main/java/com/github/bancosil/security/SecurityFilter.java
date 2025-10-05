e358accc8d296cad9020df470a12348eca2ded09
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-05 12:57:43 -0300

    feat(security): add SecurityFilter to filter login attempts

---
+++ src/main/java/com/github/bancosil/security/SecurityFilter.java
@@ -0,0 +1,49 @@
+package com.github.bancosil.security;
+
+import com.github.bancosil.exception.account.AccountNotFoundException;
+import com.github.bancosil.model.Account;
+import com.github.bancosil.repository.AccountRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+
+@Component
+@RequiredArgsConstructor
+public class SecurityFilter extends OncePerRequestFilter {
+
+    private final AccountRepository accountRepository;
+    private final TokenService tokenService;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = recoverToken(request);
+        var login = tokenService.validateToken(token);
+
+        if(login != null){
+            Account account = accountRepository.findByUsername(login).orElseThrow(AccountNotFoundException::new);
+            var authorities = Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
+            var authentication = new UsernamePasswordAuthenticationToken(account, null, authorities);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request){
+        var authHeader = request.getHeader("Authorization");
+        if(authHeader == null){
+            return null;
+        }
+        return authHeader.replace("Bearer ", "");
+    }
+}
