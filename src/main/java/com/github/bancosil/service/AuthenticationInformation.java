6f1970aa4c13790bdeb7e6320c2b576c02bc834a
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-05 12:35:02 -0300

    feat(service): add AuthenticationInformation class to get current logged user

---
+++ src/main/java/com/github/bancosil/service/AuthenticationInformation.java
@@ -0,0 +1,21 @@
+package com.github.bancosil.service;
+
+import com.github.bancosil.model.Account;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+class AuthenticationInformation {
+
+    public Account getAuthenticatedUser(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if(authentication == null || !(authentication.isAuthenticated())){
+            throw new RuntimeException();
+        }
+
+        return (Account) authentication.getPrincipal();
+    }
+
+}
