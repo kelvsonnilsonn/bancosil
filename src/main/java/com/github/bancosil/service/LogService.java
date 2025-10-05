1c95de612756bda5b2eba01cf4fba34b8e3a4a47
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-05 15:51:46 -0300

    feat(service): add findAll method to get all logs in database
    - injected LogMapper to map logs into LongResponseDTO

--- src/main/java/com/github/bancosil/service/LogService.java
+++ src/main/java/com/github/bancosil/service/LogService.java
@@ -1,22 +1,27 @@
 package com.github.bancosil.service;
 
+import com.github.bancosil.dto.LogResponseDTO;
+import com.github.bancosil.dto.PageResponseDTO;
+import com.github.bancosil.mapper.LogMapper;
 import com.github.bancosil.model.Account;
 import com.github.bancosil.model.Log;
 import com.github.bancosil.repository.LogRepository;
 import com.github.bancosil.service.operation.OperationType;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 
 @Service
+@RequiredArgsConstructor
+@Transactional
 public class LogService {
-    private final LogRepository logRepository;
 
-    @Autowired
-    public LogService(LogRepository logRepository){
-        this.logRepository = logRepository;
-    }
+    private final LogRepository logRepository;
+    private final LogMapper logMapper;
 
     public void register(OperationType operationType, Account account, BigDecimal amount) {
         Log log = new Log(operationType, account, amount);
@@ -26,5 +31,11 @@ public class LogService {
     public void register(OperationType operationType, Account sender, Account receiver, BigDecimal amount) {
         Log log = new Log(operationType, sender, receiver, amount);
         logRepository.save(log);
+    }
+
+    @Transactional(readOnly = true)
+    public PageResponseDTO<LogResponseDTO> findAll(Pageable pageable){
+        Page<LogResponseDTO> logs = logRepository.findAll(pageable).map(logMapper::toResponse);
+        return PageResponseDTO.fromPage(logs);
     }
 }
