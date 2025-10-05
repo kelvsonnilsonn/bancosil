70737e1453b11e028d768fcef8de6a682aa4e2cb
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-05 12:30:08 -0300

    refactor(repository): remove findByUsernameAndPassword method

--- src/main/java/com/github/bancosil/repository/AccountRepository.java
+++ src/main/java/com/github/bancosil/repository/AccountRepository.java
@@ -1,20 +1,15 @@
 package com.github.bancosil.repository;
 
 import com.github.bancosil.model.Account;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import com.github.bancosil.model.valueobjects.Username;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
-    @Query("SELECT a FROM Account a WHERE a.username.username = :username AND a.password.password =:password")
-    Optional<Account> findByUsernameAndPassword(@Param("username") String username, @Param("password") String password);
-
-    @Query("SELECT a FROM Account a WHERE LOWER(a.username.username) LIKE CONCAT (:username, '%')")
-    Page<Account> findByUsername(@Param("username") String username, Pageable pageable);
+    @Query("SELECT a FROM Account a WHERE a.username.username = :username")
+    Optional<Account> findByUsername(String username);
 }
