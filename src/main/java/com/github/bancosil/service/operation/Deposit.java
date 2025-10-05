9f7942091b5cd82f9b5674d05c0522f823515856
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-05 12:44:17 -0300

    refactor(service): remove validator class because amount already is not null

--- src/main/java/com/github/bancosil/service/operation/Deposit.java
+++ src/main/java/com/github/bancosil/service/operation/Deposit.java
@@ -1,14 +1,12 @@
 package com.github.bancosil.service.operation;
 
 import com.github.bancosil.model.Account;
-import com.github.bancosil.service.validator.OperationValidator;
 
 import java.math.BigDecimal;
 
 public class Deposit implements Operation{
     @Override
     public void execute(Account account, BigDecimal amount) {
-        OperationValidator.validate(amount);
         account.deposit(amount);
     }
 
