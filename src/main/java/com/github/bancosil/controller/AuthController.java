b1409a07f28f65bc02caf7c3186de3834c1819d5
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-05 12:34:37 -0300

    feat(controller): add register endpoint in AuthController
    - add @RequiredArgsConstructor annotation
    - add SecurityService injection

--- src/main/java/com/github/bancosil/controller/AuthController.java
+++ src/main/java/com/github/bancosil/controller/AuthController.java
@@ -1,12 +1,11 @@
 package com.github.bancosil.controller;
 
-import com.github.bancosil.config.AccountConfigurations;
-import com.github.bancosil.dto.AccountResponseDTO;
-import com.github.bancosil.mapper.AccountMapper;
+import com.github.bancosil.dto.auth.AuthResponseDTO;
+import com.github.bancosil.dto.auth.LoginRequestDTO;
+import com.github.bancosil.dto.auth.RegisterRequestDTO;
+import com.github.bancosil.service.SecurityService;
 import com.github.bancosil.util.AppConstants;
-import com.github.bancosil.dto.AccountRequestDTO;
-import com.github.bancosil.dto.LoginDTO;
-import com.github.bancosil.model.Account;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,25 +14,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(AppConstants.AUTH_BASE_PATH)
+@RequiredArgsConstructor
 public class AuthController implements AuthAPI{
 
-    private final AccountConfigurations accountConfigurations;
-
-    public AuthController(AccountConfigurations accountConfigurations) {
-        this.accountConfigurations = accountConfigurations;
-    }
+    private final SecurityService securityService;
 
     @Override
     @PostMapping(AppConstants.LOGIN_PATH)
-    public ResponseEntity<AccountResponseDTO> login(@RequestBody LoginDTO loginDTO) {
-        Account account = accountConfigurations.login(loginDTO.username(), loginDTO.password());
-        return ResponseEntity.ok(AccountMapper.toResponse(account));
+    public ResponseEntity<AuthResponseDTO> login(@RequestBody LoginRequestDTO loginRequestDTO) {
+        AuthResponseDTO response = securityService.login(loginRequestDTO);
+        return ResponseEntity.ok(response);
     }
 
-    @Override
-    @PostMapping(AppConstants.LOGOUT_PATH)
-    public ResponseEntity<String> logout(){
-        accountConfigurations.logout();
-        return ResponseEntity.ok("Usuário deslogado.");
+    @PostMapping(AppConstants.REGISTER_PATH)
+    public ResponseEntity<AuthResponseDTO> register(@RequestBody RegisterRequestDTO registerRequestDTO){
+        AuthResponseDTO response = securityService.register(registerRequestDTO);
+        return ResponseEntity.ok(response);
     }
+
 }
