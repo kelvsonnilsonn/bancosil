77140a251263d8a3d1df6acd4055f37db78050b9
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-05 13:36:59 -0300

    refactor(controller): add @RequiredArgsConstructor to remove explicit constructor
    - remove AccountService injection

--- src/main/java/com/github/bancosil/controller/OperationController.java
+++ src/main/java/com/github/bancosil/controller/OperationController.java
@@ -3,27 +3,22 @@ package com.github.bancosil.controller;
 import com.github.bancosil.dto.operation.DepositDTO;
 import com.github.bancosil.dto.operation.TransferDTO;
 import com.github.bancosil.dto.operation.WithdrawDTO;
-import com.github.bancosil.model.Account;
 import com.github.bancosil.service.AccountService;
 import com.github.bancosil.service.OperationalService;
 import com.github.bancosil.util.AppConstants;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.math.BigDecimal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(AppConstants.OPERATION_BASE_PATH)
+@RequiredArgsConstructor
 public class OperationController implements OperationAPI{
 
     private final OperationalService operationalService;
-    private final AccountService accountService;
-
-    public OperationController(OperationalService operationalService,
-                               AccountService accountService){
-        this.accountService = accountService;
-        this.operationalService = operationalService;
-    }
 
     @PostMapping(AppConstants.DEPOSIT_PATH)
     public ResponseEntity<String> deposit(@RequestBody DepositDTO depositDTO){
@@ -41,8 +36,7 @@ public class OperationController implements OperationAPI{
     @Override
     @PostMapping(AppConstants.TRANSFER_PATH)
     public ResponseEntity<String> transfer(@RequestBody TransferDTO transferDTO){
-        Account receiver = accountService.findEntityById(transferDTO.id());
-        operationalService.transferPix(receiver, transferDTO.amount());
+        operationalService.transferPix(transferDTO);
         return ResponseEntity.ok(AppConstants.TRANSFER_MSG);
     }
 }
