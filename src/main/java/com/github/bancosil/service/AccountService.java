f3cbb9242a7081217a608e0a5c4588c5e9f146d5
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-05 12:25:38 -0300

    refactor(service): add @Transactional(readOnly=true) in AccountService class to economize memory and faster entity search.
    - remove 'create' method because SecurityService already do this.
    - add @RequireArgsConstructor to remove explicit constructor
    - add AccountMapper mapper to fast conversions from Account to AccountResponseDTO

--- src/main/java/com/github/bancosil/service/AccountService.java
+++ src/main/java/com/github/bancosil/service/AccountService.java
@@ -1,7 +1,5 @@
 package com.github.bancosil.service;
 
-import com.github.bancosil.config.LoginSystem;
-import com.github.bancosil.dto.AccountRequestDTO;
 import com.github.bancosil.dto.AccountResponseDTO;
 import com.github.bancosil.dto.PageResponseDTO;
 import com.github.bancosil.exception.account.AccountNotFoundException;
@@ -9,73 +7,51 @@ import com.github.bancosil.mapper.AccountMapper;
 import com.github.bancosil.model.Account;
 import com.github.bancosil.repository.AccountRepository;
 import com.github.bancosil.util.AppConstants;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
 
-/**
- * Por: Kelvson Nilson
- * Última atualização: 23/09/2025
- * */
-
 @Service
-public class AccountService implements LoginSystem {
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class AccountService {
 
     private final AccountRepository accountRepository;
+    private final AccountMapper accountMapper;
 
-    @Autowired
-    public AccountService(AccountRepository accountRepository){
-        this.accountRepository = accountRepository;
-    }
-
-    public AccountResponseDTO create(AccountRequestDTO account){
-        Account receivedAccount = AccountMapper.convert(account);
-        Account savedAccount = accountRepository.save(receivedAccount);
-        return AccountMapper.toResponse(savedAccount);
-    }
-
+    @Transactional
     public String delete(Long id){
-        try{
-            Account account = findEntityById(id);
-            String message = String.format(AppConstants.ACCOUNT_DELETED_MSG, account.getUsername());
-            accountRepository.deleteById(id);
-            return message;
-        } catch (EmptyResultDataAccessException e){
-            throw new AccountNotFoundException(id);
-        }
+        Account account = findEntityById(id);
+        String message = String.format(AppConstants.ACCOUNT_DELETED_MSG, account.getUsername());
+        accountRepository.deleteById(id);
+        return message;
     }
 
-    public Account findEntityById(Long id){
-        return accountRepository.findById(id).orElseThrow(() -> new AccountNotFoundException(id));
+    public AccountResponseDTO findByUsername(String username){
+        Account account = accountRepository.findByUsername(username).orElseThrow(AccountNotFoundException::new);
+        return accountMapper.toResponse(account);
     }
 
     public AccountResponseDTO findById(Long id){
         Account account = findEntityById(id);
-        return AccountMapper.toResponse(account);
-    }
-
-    public PageResponseDTO<AccountResponseDTO> findByUsername(String username, Pageable pageable){
-        Page<AccountResponseDTO> content =  accountRepository.findByUsername(username, pageable).map(AccountMapper::toResponse);
-        return PageResponseDTO.fromPage(content);
+        return accountMapper.toResponse(account);
     }
 
     public PageResponseDTO<AccountResponseDTO> findAll(Pageable pageable){
-        Page<AccountResponseDTO> content = accountRepository.findAll(pageable).map(AccountMapper::toResponse);
+        Page<AccountResponseDTO> content = accountRepository.findAll(pageable).map(accountMapper::toResponse);
         return PageResponseDTO.fromPage(content);
     }
 
-    @Override
-    public Account login(String username, String password){
-        return accountRepository.findByUsernameAndPassword(username, password)
-                .orElseThrow(AccountNotFoundException::new);
+    public Account findEntityById(Long id){
+        return accountRepository.findById(id).orElseThrow((AccountNotFoundException::new));
     }
 
-    public void update(Account ...account) {
+    @Transactional
+    public void update(Account ...account){
         Arrays.stream(account).forEach(accountRepository::save);
     }
-
 }
