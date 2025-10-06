bc8f861ee1cc6743abbb23b2fc655f64a92c663b
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-05 21:12:53 -0300

    feat(service): add findByAuthorId method to get all user's log

--- src/main/java/com/github/bancosil/service/LogService.java
+++ src/main/java/com/github/bancosil/service/LogService.java
@@ -2,9 +2,13 @@ package com.github.bancosil.service;
 
 import com.github.bancosil.dto.LogResponseDTO;
 import com.github.bancosil.dto.PageResponseDTO;
+import com.github.bancosil.enums.AccountType;
+import com.github.bancosil.exception.account.AccountNotFoundException;
+import com.github.bancosil.exception.account.UnauthorizedException;
 import com.github.bancosil.mapper.LogMapper;
 import com.github.bancosil.model.Account;
 import com.github.bancosil.model.Log;
+import com.github.bancosil.repository.AccountRepository;
 import com.github.bancosil.repository.LogRepository;
 import com.github.bancosil.service.operation.OperationType;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +18,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -22,6 +27,7 @@ public class LogService {
 
     private final LogRepository logRepository;
     private final LogMapper logMapper;
+    private final AuthenticationInformation authenticationInformation;
 
     public void register(OperationType operationType, Account account, BigDecimal amount) {
         Log log = new Log(operationType, account, amount);
@@ -35,7 +41,23 @@ public class LogService {
 
     @Transactional(readOnly = true)
     public PageResponseDTO<LogResponseDTO> findAll(Pageable pageable){
+        validateAuthorities();
         Page<LogResponseDTO> logs = logRepository.findAll(pageable).map(logMapper::toResponse);
         return PageResponseDTO.fromPage(logs);
+    }
+
+    @Transactional(readOnly = true)
+    public PageResponseDTO<LogResponseDTO> findByAuthorId(Pageable pageable, Long authorId){
+        if(!Objects.equals(authenticationInformation.getAuthenticatedUser().getId(), authorId)){
+            throw new UnauthorizedException();
+        }
+        Page<LogResponseDTO> logs = logRepository.findByAuthorId(pageable, authorId).map(logMapper::toResponse);
+        return PageResponseDTO.fromPage(logs);
+    }
+
+    private void validateAuthorities(){
+        if(authenticationInformation.getAuthenticatedUser().getType() != AccountType.ADMIN){
+            throw new UnauthorizedException();
+        }
     }
 }
