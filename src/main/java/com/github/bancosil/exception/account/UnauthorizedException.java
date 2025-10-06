2cee28cf20aba12411112d84ab0b4e475aceacca
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-05 21:12:29 -0300

    refactor(exception): change unauthorized message

--- src/main/java/com/github/bancosil/exception/account/UnauthorizedException.java
+++ src/main/java/com/github/bancosil/exception/account/UnauthorizedException.java
@@ -2,7 +2,7 @@ package com.github.bancosil.exception.account;
 
 public class UnauthorizedException extends RuntimeException {
     public UnauthorizedException() {
-        super("Usuário não logado.");
+        super("Usuário não autorizado.");
     }
 
     public UnauthorizedException(String message) {
