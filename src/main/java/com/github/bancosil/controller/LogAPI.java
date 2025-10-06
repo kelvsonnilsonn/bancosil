2202ec7c8bcd6217fa210cb50b221dc4285275e6
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-06 16:05:43 -0300

    feat(controller): define findByInterval endpoint in LogAPI

--- src/main/java/com/github/bancosil/controller/LogAPI.java
+++ src/main/java/com/github/bancosil/controller/LogAPI.java
@@ -1,13 +1,16 @@
 package com.github.bancosil.controller;
 
+import com.github.bancosil.dto.DateIntervalDTO;
 import com.github.bancosil.dto.LogResponseDTO;
 import com.github.bancosil.dto.PageResponseDTO;
 import com.github.bancosil.util.HttpConstants;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Tag(name="Logs", description = "Gestão de logs das operações")
@@ -22,4 +25,9 @@ public interface LogAPI {
     @ApiResponse(responseCode = HttpConstants.OK, description = "Logs listados com sucesso")
     @ApiResponse(responseCode = HttpConstants.SERVER_ERROR, description = HttpConstants.INTERN_SERVER_ERROR_MSG)
     ResponseEntity<PageResponseDTO<LogResponseDTO>> findAuthorLogs(Pageable pageable, @RequestParam Long id);
+
+    @Operation(summary = "Listar logs de usuário", description = "Lista todos os logs de um usuário.")
+    @ApiResponse(responseCode = HttpConstants.OK, description = "Logs listados com sucesso")
+    @ApiResponse(responseCode = HttpConstants.SERVER_ERROR, description = HttpConstants.INTERN_SERVER_ERROR_MSG)
+    ResponseEntity<PageResponseDTO<LogResponseDTO>> findByInterval(Pageable pageable, @RequestBody @Valid DateIntervalDTO dateInterval);
 }
