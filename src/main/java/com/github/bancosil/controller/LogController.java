617b29290dfc3abc236bee39e570ef822c495148
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-05 22:19:58 -0300

    feat(controller): add findByInterval endpoint to get logs between intervals

--- src/main/java/com/github/bancosil/controller/LogController.java
+++ src/main/java/com/github/bancosil/controller/LogController.java
@@ -1,16 +1,15 @@
 package com.github.bancosil.controller;
 
+import com.github.bancosil.dto.DateIntervalDTO;
 import com.github.bancosil.dto.LogResponseDTO;
 import com.github.bancosil.dto.PageResponseDTO;
 import com.github.bancosil.service.LogService;
 import com.github.bancosil.util.AppConstants;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(AppConstants.LOG_BASE_PATH)
@@ -27,5 +26,10 @@ public class LogController implements LogAPI {
     @GetMapping(AppConstants.AUTHOR_LOG_PATH)
     public ResponseEntity<PageResponseDTO<LogResponseDTO>> findAuthorLogs(Pageable pageable, @RequestParam Long id){
         return ResponseEntity.ok(logService.findByAuthorId(pageable, id));
+    }
+
+    @GetMapping(AppConstants.INTERVAL_LOG_PATH)
+    public ResponseEntity<PageResponseDTO<LogResponseDTO>> findByInterval(Pageable pageable, @RequestBody @Valid DateIntervalDTO dateInterval){
+        return ResponseEntity.ok(logService.findByInterval(pageable, dateInterval.startDate(), dateInterval.endDate()));
     }
 }
