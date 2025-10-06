d16c3f139c8b31a5acbff808b2048ee976173db4
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-05 22:20:24 -0300

    feat(repository): add findByInterval method in LogRepository

--- src/main/java/com/github/bancosil/repository/LogRepository.java
+++ src/main/java/com/github/bancosil/repository/LogRepository.java
@@ -6,8 +6,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
+
 public interface LogRepository extends JpaRepository<Log, Long> {
 
     @Query("SELECT l FROM Log l WHERE l.author.id = :authorId")
     Page<Log> findByAuthorId(Pageable pageable, Long authorId);
+
+    @Query("SELECT l FROM Log l WHERE l.createdAt BETWEEN :startDate AND :endDate")
+    Page<Log>  findByInterval(Pageable pageable, LocalDateTime startDate, LocalDateTime endDate);
 }
