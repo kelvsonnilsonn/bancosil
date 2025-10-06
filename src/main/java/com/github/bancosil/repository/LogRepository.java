14bb33deeff911d84a67eb52f39867d721c6324b
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-05 21:13:23 -0300

    feat(repository): add findByAuthorId method to get all user's log

--- src/main/java/com/github/bancosil/repository/LogRepository.java
+++ src/main/java/com/github/bancosil/repository/LogRepository.java
@@ -1,7 +1,13 @@
 package com.github.bancosil.repository;
 
 import com.github.bancosil.model.Log;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface LogRepository extends JpaRepository<Log, Long> {
+
+    @Query("SELECT l FROM Log l WHERE l.author.id = :authorId")
+    Page<Log> findByAuthorId(Pageable pageable, Long authorId);
 }
