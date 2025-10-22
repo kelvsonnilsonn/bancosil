5d3676160e12fb211c15b5e9fd2060c239a3c704
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-22 15:34:45 -0300

    feat(util): add event paths

--- src/main/java/com/github/bancosil/util/AppConstants.java
+++ src/main/java/com/github/bancosil/util/AppConstants.java
@@ -9,9 +9,8 @@ public class AppConstants {
 
     public static final String ACCOUNT_BASE_PATH = "/accounts";
     public static final String ID_PATH = "/{id}";
-    public static final String SEARCH_PATH = "/search";
     public static final String ADMIN_PATH = "/admin";
-
+    public static final String EVENT_BASE_PATH = "/events";
 
     public static final String AUTH_BASE_PATH = "/auth";
     public static final String LOGIN_PATH = "/login";
@@ -22,9 +21,9 @@ public class AppConstants {
     public static final String WITHDRAW_PATH = "/withdraw";
     public static final String TRANSFER_PATH = "/transfer";
 
-    public static final String LOG_BASE_PATH = "/logs";
-    public static final String AUTHOR_LOG_PATH = "/author";
-    public static final String INTERVAL_LOG_PATH = "/interval";
+    public static final String EVENTS_IN_INTERVAL_PATH = "/interval";
+    public static final String MY_EVENTS_IN_INTERVAL_PATH = "/my-events/interval";
+    public static final String USER_EVENTS_IN_INTERVAL_PATH = "/user-events/interval";
 
 
     // Parâmetros
@@ -40,5 +39,8 @@ public class AppConstants {
 
     public static final String PRE_AUTHORIZE_ADMIN_REQUISITION = "hasAuthority('ADMIN_ROLE')";
     public static final String PRE_AUTHORIZE_ALL_REQUISITION = "permitAll()";
+
+    public static final String OPERATION = "OPERATION";
+    public static final String ACCOUNT = "ACCOUNT";
 
 }
