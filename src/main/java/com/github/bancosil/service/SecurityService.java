b25c3be2a8c0360b34e591419bc80ab0d07e9c71
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-22 12:05:58 -0300

    refactor(service): change register method parameter from RegisterRequestDTO to CreateAccountCommand

--- src/main/java/com/github/bancosil/service/SecurityService.java
+++ src/main/java/com/github/bancosil/service/SecurityService.java
@@ -1,16 +1,17 @@
 package com.github.bancosil.service;
 
-import com.github.bancosil.dto.auth.AuthResponseDTO;
-import com.github.bancosil.dto.auth.LoginRequestDTO;
-import com.github.bancosil.dto.auth.RegisterRequestDTO;
+import com.github.bancosil.command.account.CreateAccountCommand;
+import com.github.bancosil.dto.AuthResponseDTO;
+import com.github.bancosil.command.auth.LoginCommand;
 import com.github.bancosil.enums.AccountType;
 import com.github.bancosil.exception.AccountAlreadyExistsException;
 import com.github.bancosil.exception.FailedLoginAttemptException;
 import com.github.bancosil.exception.account.AccountNotFoundException;
 import com.github.bancosil.model.Account;
-import com.github.bancosil.model.valueobjects.Email;
-import com.github.bancosil.model.valueobjects.Password;
-import com.github.bancosil.model.valueobjects.Username;
+import com.github.bancosil.model.valueobjects.UserInformation;
+import com.github.bancosil.model.valueobjects.account.Email;
+import com.github.bancosil.model.valueobjects.account.Password;
+import com.github.bancosil.model.valueobjects.account.Username;
 import com.github.bancosil.model.valueobjects.cpfchecker.CPF;
 import com.github.bancosil.repository.AccountRepository;
 import com.github.bancosil.security.TokenService;
@@ -18,7 +19,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Optional;
 
@@ -32,7 +32,7 @@ public class SecurityService {
     private final TokenService tokenService;
 
     @Transactional(readOnly = true)
-    public AuthResponseDTO login(@RequestBody LoginRequestDTO dto){
+    public AuthResponseDTO login(LoginCommand dto){
         Account account = accountRepository.findByUsername(dto.username()).orElseThrow(AccountNotFoundException::new);
         if(!(passwordEncoder.matches(dto.password(), account.getPassword()))){
             throw new FailedLoginAttemptException();
@@ -41,23 +41,24 @@ public class SecurityService {
         return new AuthResponseDTO(token, account.getUsername());
     }
 
-    public AuthResponseDTO register(@RequestBody RegisterRequestDTO dto){
-        Optional<Account> account = accountRepository.findByUsername(dto.username());
+    public AuthResponseDTO register(CreateAccountCommand command){
+        Optional<Account> account = accountRepository.findByUsername(command.username());
         if(account.isPresent()){
             throw new AccountAlreadyExistsException();
         }
-        Account newAccount = createAccount(dto);
+        Account newAccount = createAccount(command);
         accountRepository.save(newAccount);
         String token = tokenService.generateToken(newAccount);
         return new AuthResponseDTO(token, newAccount.getUsername());
     }
 
-    private Account createAccount(RegisterRequestDTO dto){
-        Username username = new Username(dto.username());
-        Password password = Password.of(dto.password(), passwordEncoder);
-        CPF cpf = new CPF(dto.cpf());
-        AccountType type = AccountType.valueOf(dto.type().toUpperCase());
-        Email email = new Email(dto.email());
-        return new Account(username, password, email, cpf, type);
+    private Account createAccount(CreateAccountCommand command){
+        Username username = new Username(command.username());
+        Password password = Password.of(command.password(), passwordEncoder);
+        CPF cpf = new CPF(command.cpf());
+        AccountType type = AccountType.valueOf(command.type().toUpperCase());
+        Email email = new Email(command.email());
+        UserInformation userInformation = new UserInformation(username, password, email, cpf);
+        return new Account(userInformation, type);
     }
 }
