162c53a961a0a534710d5891598eb3ab2ae1a1b0
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-22 12:12:44 -0300

    refactor(repository): update findByUsername query
    - use native query instead of JPA query

--- src/main/java/com/github/bancosil/repository/AccountRepository.java
+++ src/main/java/com/github/bancosil/repository/AccountRepository.java
@@ -1,15 +1,13 @@
 package com.github.bancosil.repository;
 
 import com.github.bancosil.model.Account;
-import com.github.bancosil.model.valueobjects.Username;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
-    @Query("SELECT a FROM Account a WHERE a.username.username = :username")
+    @Query(value = "SELECT * FROM accounts WHERE username =:username",nativeQuery = true)
     Optional<Account> findByUsername(String username);
 }
