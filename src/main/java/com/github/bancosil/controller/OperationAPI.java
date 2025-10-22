07ff1f54b8af1671df56b64f5bacdb752d3eebf4
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-22 12:11:17 -0300

    feat(controller): add @PreAuthorize in OperationController and add @Valid annotation in endpoints parameters to validate inputs

--- src/main/java/com/github/bancosil/controller/OperationAPI.java
+++ src/main/java/com/github/bancosil/controller/OperationAPI.java
@@ -1,16 +1,17 @@
 package com.github.bancosil.controller;
 
-import com.github.bancosil.dto.operation.DepositDTO;
-import com.github.bancosil.dto.operation.TransferDTO;
-import com.github.bancosil.dto.operation.WithdrawDTO;
+import com.github.bancosil.command.operation.DepositCommand;
+import com.github.bancosil.command.operation.TransferCommand;
+import com.github.bancosil.command.operation.WithdrawCommand;
 import com.github.bancosil.util.HttpConstants;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@Tag(name = "Operations", description = "Operações que um usuário pode fazer.")
+@Tag(name = "Operações", description = "Operações que um usuário pode fazer.")
 public interface OperationAPI {
 
     @Operation(summary = "Depósitar valor", description = "Realiza um depósito na conta do usuário logado")
@@ -18,14 +19,14 @@ public interface OperationAPI {
     @ApiResponse(responseCode = HttpConstants.UNAUTHORIZED, description = HttpConstants.UNAUTHORIZED_MSG)
     @ApiResponse(responseCode = HttpConstants.BAD_REQUEST, description = "Valor inválido, negativo ou zero")
     @ApiResponse(responseCode = HttpConstants.SERVER_ERROR, description = HttpConstants.INTERN_SERVER_ERROR_MSG)
-    ResponseEntity<String> deposit(@RequestBody DepositDTO depositDTO);
+    ResponseEntity<String> deposit(@RequestBody @Valid DepositCommand depositCommand);
 
     @Operation(summary = "Sacar um valor", description = "Saca um valor da conta do usuário logado")
     @ApiResponse(responseCode = HttpConstants.OK, description = "Saque realizado com sucesso")
     @ApiResponse(responseCode = HttpConstants.UNAUTHORIZED, description = HttpConstants.UNAUTHORIZED_MSG)
     @ApiResponse(responseCode = HttpConstants.BAD_REQUEST, description = "Valor inválido, negativo, zero ou saldo insuficiente")
     @ApiResponse(responseCode = HttpConstants.SERVER_ERROR, description = HttpConstants.INTERN_SERVER_ERROR_MSG)
-    ResponseEntity<String> withdraw(@RequestBody WithdrawDTO withdrawDTO);
+    ResponseEntity<String> withdraw(@RequestBody @Valid WithdrawCommand withdrawCommand);
 
     @Operation(summary = "Transferir um valor", description = "Transfere um valor da conta logada para outra conta")
     @ApiResponse(responseCode = HttpConstants.OK, description = "Transferência realizada com sucesso")
@@ -33,5 +34,5 @@ public interface OperationAPI {
     @ApiResponse(responseCode = HttpConstants.BAD_REQUEST, description = "Valor inválido, negativo, zero, saldo insuficiente ou transferência para si mesmo")
     @ApiResponse(responseCode = HttpConstants.NOT_FOUND, description = "Conta destino não encontrada")
     @ApiResponse(responseCode = HttpConstants.SERVER_ERROR, description = HttpConstants.INTERN_SERVER_ERROR_MSG)
-    ResponseEntity<String> transfer(@RequestBody TransferDTO transferDTO);
+    ResponseEntity<String> transfer(@RequestBody @Valid TransferCommand transferCommand);
 }
--- src/main/java/com/github/bancosil/controller/OperationController.java
+++ src/main/java/com/github/bancosil/controller/OperationController.java
@@ -1,13 +1,14 @@
 package com.github.bancosil.controller;
 
-import com.github.bancosil.dto.operation.DepositDTO;
-import com.github.bancosil.dto.operation.TransferDTO;
-import com.github.bancosil.dto.operation.WithdrawDTO;
-import com.github.bancosil.service.AccountService;
+import com.github.bancosil.command.operation.DepositCommand;
+import com.github.bancosil.command.operation.TransferCommand;
+import com.github.bancosil.command.operation.WithdrawCommand;
 import com.github.bancosil.service.OperationalService;
 import com.github.bancosil.util.AppConstants;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,27 +17,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(AppConstants.OPERATION_BASE_PATH)
 @RequiredArgsConstructor
+@PreAuthorize(AppConstants.PRE_AUTHORIZE_ALL_REQUISITION)
 public class OperationController implements OperationAPI{
 
     private final OperationalService operationalService;
 
     @PostMapping(AppConstants.DEPOSIT_PATH)
-    public ResponseEntity<String> deposit(@RequestBody DepositDTO depositDTO){
-        operationalService.deposit(depositDTO.amount());
+    public ResponseEntity<String> deposit(@RequestBody @Valid DepositCommand depositCommand){
+        operationalService.deposit(depositCommand.amount());
         return ResponseEntity.ok(AppConstants.DEPOSIT_MSG);
     }
 
     @Override
     @PostMapping(AppConstants.WITHDRAW_PATH)
-    public ResponseEntity<String> withdraw(@RequestBody WithdrawDTO withdrawDTO){
-        operationalService.withdraw(withdrawDTO.amount());
+    public ResponseEntity<String> withdraw(@RequestBody @Valid WithdrawCommand withdrawCommand){
+        operationalService.withdraw(withdrawCommand.amount());
         return ResponseEntity.ok(AppConstants.WITHDRAW_MSG);
     }
 
     @Override
     @PostMapping(AppConstants.TRANSFER_PATH)
-    public ResponseEntity<String> transfer(@RequestBody TransferDTO transferDTO){
-        operationalService.transferPix(transferDTO);
+    public ResponseEntity<String> transfer(@RequestBody @Valid TransferCommand transferCommand){
+        operationalService.transferPix(transferCommand);
         return ResponseEntity.ok(AppConstants.TRANSFER_MSG);
     }
 }
