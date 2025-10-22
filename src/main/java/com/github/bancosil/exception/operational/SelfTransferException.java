80c617e38bf8a4c5ca1c4cca7ed4d3522478594c
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-22 12:02:24 -0300

    feat(exception): add new personalized exception

--- src/main/java/com/github/bancosil/exception/operational/SelfTransferException.java
+++ src/main/java/com/github/bancosil/exception/operational/SelfTransferException.java
@@ -1,6 +1,7 @@
 package com.github.bancosil.exception.operational;
 
 public class SelfTransferException extends RuntimeException {
+    public SelfTransferException() { super("Você não pode transferir para si mesmo");}
     public SelfTransferException(String message) {
         super(message);
     }
