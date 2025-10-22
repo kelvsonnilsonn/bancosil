e0d9eef9217d3b69dbcdc4e68ad41c537d207838
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-10-22 12:11:34 -0300

    feat(controller): add @PreAuthorize in AuthController and add @Valid annotation in endpoints parameters to validate inputs

--- src/main/java/com/github/bancosil/controller/AuthAPI.java
+++ src/main/java/com/github/bancosil/controller/AuthAPI.java
@@ -1,12 +1,13 @@
 package com.github.bancosil.controller;
 
-import com.github.bancosil.dto.auth.AuthResponseDTO;
-import com.github.bancosil.dto.auth.LoginRequestDTO;
-import com.github.bancosil.dto.auth.RegisterRequestDTO;
+import com.github.bancosil.command.account.CreateAccountCommand;
+import com.github.bancosil.dto.AuthResponseDTO;
+import com.github.bancosil.command.auth.LoginCommand;
 import com.github.bancosil.util.HttpConstants;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -18,10 +19,10 @@ public interface AuthAPI {
     @ApiResponse(responseCode = HttpConstants.UNAUTHORIZED, description = "Credenciais inválidas")
     @ApiResponse(responseCode = HttpConstants.BAD_REQUEST, description = HttpConstants.BAD_REQUEST_MSG)
     @ApiResponse(responseCode = HttpConstants.SERVER_ERROR, description = HttpConstants.INTERN_SERVER_ERROR_MSG)
-    ResponseEntity<AuthResponseDTO> login(@RequestBody LoginRequestDTO loginRequestDTO);
+    ResponseEntity<AuthResponseDTO> login(@RequestBody @Valid LoginCommand loginCommand);
 
     @Operation(summary = "Registrar usuário", description = "Registra um usuário no sistema")
     @ApiResponse(responseCode = HttpConstants.OK, description = "Registrado com sucesso")
     @ApiResponse(responseCode = HttpConstants.SERVER_ERROR, description = HttpConstants.INTERN_SERVER_ERROR_MSG)
-    ResponseEntity<AuthResponseDTO> register(@RequestBody RegisterRequestDTO registerRequestDTO);
+    ResponseEntity<AuthResponseDTO> register(@RequestBody @Valid CreateAccountCommand command);
 }
--- src/main/java/com/github/bancosil/controller/AuthController.java
+++ src/main/java/com/github/bancosil/controller/AuthController.java
@@ -1,12 +1,14 @@
 package com.github.bancosil.controller;
 
-import com.github.bancosil.dto.auth.AuthResponseDTO;
-import com.github.bancosil.dto.auth.LoginRequestDTO;
-import com.github.bancosil.dto.auth.RegisterRequestDTO;
+import com.github.bancosil.command.account.CreateAccountCommand;
+import com.github.bancosil.dto.AuthResponseDTO;
+import com.github.bancosil.command.auth.LoginCommand;
 import com.github.bancosil.service.SecurityService;
 import com.github.bancosil.util.AppConstants;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,21 +17,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(AppConstants.AUTH_BASE_PATH)
 @RequiredArgsConstructor
+@PreAuthorize(AppConstants.PRE_AUTHORIZE_ALL_REQUISITION)
 public class AuthController implements AuthAPI{
 
     private final SecurityService securityService;
 
     @Override
     @PostMapping(AppConstants.LOGIN_PATH)
-    public ResponseEntity<AuthResponseDTO> login(@RequestBody LoginRequestDTO loginRequestDTO) {
-        AuthResponseDTO response = securityService.login(loginRequestDTO);
+    public ResponseEntity<AuthResponseDTO> login(@RequestBody @Valid LoginCommand loginCommand) {
+        AuthResponseDTO response = securityService.login(loginCommand);
         return ResponseEntity.ok(response);
     }
 
     @PostMapping(AppConstants.REGISTER_PATH)
-    public ResponseEntity<AuthResponseDTO> register(@RequestBody RegisterRequestDTO registerRequestDTO){
-        AuthResponseDTO response = securityService.register(registerRequestDTO);
-        return ResponseEntity.ok(response);
+    public ResponseEntity<AuthResponseDTO> register(@RequestBody @Valid CreateAccountCommand command){
+        return ResponseEntity.ok(securityService.register(command));
     }
 
 }
